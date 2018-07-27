552d96f95d1c169e0db730e4d5c5450f1e9f9d98
comptegithubprojet <37439298+comptegithubprojet@users.noreply.github.com>
2018-07-27 15:35:13 +0200

    Add files via upload
    Ajout du CDN Bootstrap dans l'application 
    Modification de la page show de l'app frontend avec des popup de confirmation pour la suppression et signalement des commentaires

--- Application/App/Frontend/Modules/News/Views/show.php
+++ Application/App/Frontend/Modules/News/Views/show.php
@@ -21,15 +21,66 @@ foreach ($comments as $comment)
 ?>
 <fieldset>
   <legend>
+
+    <div class="container">      
+      <div id="html">
+
     Posté par <strong><?= htmlspecialchars($comment['auteur']) ?></strong> le <?= $comment['date']->format('d/m/Y à H\hi') ?>
-    <?php if ($user->isAuthenticated()) { ?> -
-      <a href="admin/comment-update-<?= $comment['id'] ?>.html">Modifier</a> |
-      <a href="admin/comment-delete-<?= $comment['id'] ?>.html">Supprimer</a>
+    <?php if ($user->isAuthenticated()) { ?> 
+
+      -
+
+      <a href="admin/comment-update-<?= $comment['id'] ?>.html">Modifier</a> 
+
+      |
+
+      <a data-toggle="modal" href="#delete<?= $comment['id']?>">Supprimer</a>
+        <div class="modal fade" id="delete<?= $comment['id']?>">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Etes vous sur de vouloir supprimer le commentaire  de <?= $comment['auteur']?> ?</h4>
+              </div>
+              <div class="modal-body">
+                <?= $comment['contenu']?>
+              </div>
+              <div class="modal-footer">
+                <a id="btn_modal_comment" role="button" class="btn btn-danger" href="admin/comment-delete-<?= $comment['id'] ?>.html">Supprimer</a>
+                <button class="btn btn-default" data-dismiss="modal">Annuler</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
     <?php } ?>
-    - <a href="comment-signal-<?= $comment['id'] ?>.html">Signaler</a>
+
+    - 
+    
+        <a data-toggle="modal" href="#signaler<?= $comment['id']?>">Signaler</a>
+        <div class="modal fade" id="signaler<?= $comment['id']?>">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Etes vous sur de vouloir signaler le commentaire  de <?= $comment['auteur']?> ?</h4>
+              </div>
+              <div class="modal-body">
+                <?= $comment['contenu']?>
+              </div>
+              <div class="modal-footer">
+                <a id="btn_modal_comment" role="button" class="btn btn-danger" href="comment-signal-<?= $comment['id'] ?>.html">Signaler</a>
+                <button class="btn btn-default" data-dismiss="modal">Annuler</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </legend>
   <p><?= nl2br(htmlspecialchars($comment['contenu'])) ?></p>
 </fieldset>
+
 <?php
 }
 ?>
--- Application/App/Frontend/Templates/layout.php
+++ Application/App/Frontend/Templates/layout.php
@@ -6,8 +6,8 @@
     </title>
  
     <meta charset="utf-8" />
- 
-    <link rel="stylesheet" href="/css/Envision.css" type="text/css" />
+    <link rel="stylesheet" href="/css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="/css/Envision.css" type="text/css"/>
   </head>
  
   <body>
@@ -39,9 +39,17 @@
  
       <footer></footer>
     </div>
-
     <script type="text/javascript" src="/js/jquery.min.js"></script>
     <script type="text/javascript" src="/plugin/tinymce/tinymce.min.js"></script>
     <script type="text/javascript" src="/plugin/tinymce/init-tinymce.js"></script>
   </body>
 </html>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
